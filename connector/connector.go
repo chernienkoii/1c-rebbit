dfe44954f1277f353822b8acde5316d1cde6089c
MD Admin 03 <mdadmin03@md-group.net.ua>
2021-07-08 15:53:25 +0300

    change git

--- connector/connector.go
+++ connector/connector.go
@@ -67,8 +67,7 @@ func (Connector *Connector) ConsumeFromQueue() (map[string]rootsctuct.Customer_s
 		false,                   // exclusive
 		false,                   // no-wait
 		nil,                     // arguments
-	)
-	q, err := Connector.RabbitMQ_channel.QueueDeclare(
+
 		"sku", // name
 		false,                   // durable
 		false,                   // delete when unused
@@ -155,8 +154,7 @@ func (Connector *Connector) SendInQueue(Customer_struct rootsctuct.Customer_stru
 		false,                   // exclusive
 		false,                   // no-wait
 		nil,                     // arguments
-	)
-	q, err := Connector.RabbitMQ_channel.QueueDeclare(
+
 		"sku", // name
 		false,                   // durable
 		false,                   // delete when unused
